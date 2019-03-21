4d54c99eb1d1233ce9fcdd15baad2032128d2999
vagrant <tp.bowes@gmail.com>
2019-03-21 19:47:15 +0000

    fixed unrelated bug

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,8 @@
 class ProductsController < ApplicationController
 
-  # def index
-  #   @products = Product.all.order(created_at: :desc)
-  # end
+  def index
+    @products = Product.all.order(created_at: :desc)
+  end
 
   def show
     @product = Product.find params[:id]
