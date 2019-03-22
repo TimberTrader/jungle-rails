8b1361be7ca0707f49f677d939b472dbeb83b13d
vagrant <tp.bowes@gmail.com>
2019-03-22 23:53:51 +0000

    fixed issues re users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  attr_accessor :first_name, :last_name, :email, :password_digest
+  # attr_accessor :first_name, :last_name, :email, :password_digest
   
-  validates_uniqueness_of :email
+  #validates_uniqueness_of :email
 end
