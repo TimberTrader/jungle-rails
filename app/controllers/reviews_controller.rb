c8860dcffb8e33392ab29bfa06f5002751bc5174
vagrant <tp.bowes@gmail.com>
2019-03-24 01:00:57 +0000

    implemented user-post-review and reordered reviews in descending order

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,19 @@
 class ReviewsController < ApplicationController
+  
+  def new
+    @review = Review.new
+  end
 
   def create
+    review = current_user.reviews.create(review_params)
+    review.save
+    redirect_to :back
+  end
+
+  private
+  def review_params
+    params.require(:review)
+    params.permit(:product_id).merge(params.require(:review).permit(:rating, :description))
   end
 
 end
