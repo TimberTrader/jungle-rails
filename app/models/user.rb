4137b9783f3e9d9d55004a6fdc575ce712ecdc23
vagrant <tp.bowes@gmail.com>
2019-03-26 14:18:25 +0000

    added authenticate with credentials & tested for edger cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,17 @@ class User < ActiveRecord::Base
   
   has_secure_password
   
-  validates_uniqueness_of :email
+  validates :email, presence: true, :uniqueness => {:case_sensitive => false}
 
   validates_presence_of :first_name, :last_name, :email
 
-  validates :password, :presence => true,
-                       :confirmation => true,
-                       :length => {:minimum => 4}
-
-
-
+  validates :password, :presence => true, :confirmation => true, :length => {:minimum => 4}
+  
+  def self.authenticate_with_credentials(email, password)
+    user = User.where("lower(email) = ?", email.strip.downcase).first unless email == nil
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -57,6 +57,26 @@ RSpec.describe User, type: :model do
     expect(user2.errors.full_messages).to include "Email has already been taken"
   end
 
+  it "is not valid when email is not unique"  do
+    user1 = User.new(
+      first_name: "pete",
+      last_name: "peters",
+      email: "pp@mail.com",
+      password: "abcde",
+      password_confirmation: "12345"
+    )
+    user2 = User.new(
+    first_name: "jan",
+    last_name: "junes",
+    email: "pp@mail.com",
+    password: "ABCDE",
+    password_confirmation: "12345"
+    )
+    user1.save
+    user2.save
+    expect(user2.errors.full_messages).to include "Email has already been taken"
+  end
+
   it "is not valid when first name is not present"  do
       user = User.new(
         first_name: nil,
@@ -103,5 +123,48 @@ RSpec.describe User, type: :model do
     )
     user.save
     expect(user.errors.full_messages).to include "Password is too short (minimum is 4 characters)"
+  end
+
+  describe '.authenticate_with_credentials' do
+    before(:each) do
+      @user = User.new({
+        first_name: "pete",
+        last_name: "peters",
+        email: "pp@mail.com",
+        password: "abcde",
+        password_confirmation: "abcde"
+      })
+      @user.save!
+    end
+
+    after(:each) do
+      @user.destroy
+      remove_instance_variable(:@user)
+    end
+
+    it "is valid when guser gives the correct credentials" do
+      user = User.authenticate_with_credentials(@user.email, 'testtest')
+      expect(user).to eq(@user)
+    end
+
+    it "is not vaild when user gives the incorrect credentials" do
+      user = User.authenticate_with_credentials(@user.email, 'no match')
+      expect(user).to be_nil
+    end
+
+    it "is not vaild when missing credentials" do
+      user = User.authenticate_with_credentials(nil, nil)
+      expect(user).to be_nil
+    end
+
+    it "is vaild with a padded emails" do
+      user = User.authenticate_with_credentials('    ' + @user.email, 'testtest')
+      expect(user).to eq(@user)
+    end
+
+    it " is vaild with incorrect case email" do
+      user = User.authenticate_with_credentials(@user.email.upcase, 'testtest')
+      expect(user).to eq(@user)
+    end
   end
 end
