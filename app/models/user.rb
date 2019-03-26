175a3111a88d6862cfbb0779c4c0d6483ecabe40
vagrant <tp.bowes@gmail.com>
2019-03-26 02:21:07 +0000

    validations for pw = pw confirmation, uniquenes of email & presence of names, emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,13 @@ class User < ActiveRecord::Base
   has_many :reviews
   
   has_secure_password
-  # attr_accessor :first_name, :last_name, :email, :password_digest
   
   validates_uniqueness_of :email
+
+  validates_presence_of :first_name, :last_name, :email, :password, :password_confirmation
+  #validates :last_name, presence true
+  #validates :email, presence true
+  #validates :paswword, presence true
+  #validates :password_confirmation, presence true
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,96 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "is valid with all fields"  do
+    user = User.new(
+    first_name: "pete",
+    last_name: "peters",
+    email: "pp@mail.com",
+    password: "12345",
+    password_confirmation: "12345"
+    )
+    expect(user.save).to eql true
+  end
+
+  it "is not valid when passwords do not match"  do
+    user = User.new(
+    first_name: "pete",
+    last_name: "peters",
+    email: "pp@mail.com",
+    password: "12345",
+    password_confirmation: "56768"
+    )
+    user.save
+    expect(user.errors.full_messages).to include "Password confirmation doesn't match Password"
+  end
+
+  it "is not valid when passwords do not match"  do
+    user = User.new(
+    first_name: "pete",
+    last_name: "peters",
+    email: "pp@mail.com",
+    password: "12345",
+    password_confirmation: "56768"
+    )
+    user.save
+    expect(user.errors.full_messages).to include "Password confirmation doesn't match Password"
+  end
+
+  it "is not valid when email is not unique"  do
+    user1 = User.new(
+      first_name: "pete",
+      last_name: "peters",
+      email: "pp@mail.com",
+      password: "12345",
+      password_confirmation: "12345"
+    )
+    user2 = User.new(
+    first_name: "jan",
+    last_name: "junes",
+    email: "pp@mail.com",
+    password: "12345",
+    password_confirmation: "12345"
+    )
+    user1.save
+    user2.save
+    expect(user2.errors.full_messages).to include "Email has already been taken"
+  end
+
+  it "is not valid when first name is not present"  do
+      user = User.new(
+        first_name: nil,
+        last_name: "peters",
+        email: "pp@mail.com",
+        password: "12345",
+        password_confirmation: "12345"
+      )
+      user.save
+      expect(user.errors.full_messages).to include "First name can't be blank"
+  end
+
+  it "is not valid when last name is not present"  do
+    user = User.new(
+      first_name: "pete",
+      last_name: nil,
+      email: "pp@mail.com",
+      password: "12345",
+      password_confirmation: "12345"
+    )
+    user.save
+    expect(user.errors.full_messages).to include "Last name can't be blank"
+  end
+
+  it "is not valid when email is not present"  do
+    user = User.new(
+      first_name: "pete",
+      last_name: "peters",
+      email: nil,
+      password: "12345",
+      password_confirmation: "12345"
+    )
+    user.save
+    expect(user.errors.full_messages).to include "Email can't be blank"
+  end
+
 end
